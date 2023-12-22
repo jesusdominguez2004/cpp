d8c393c13bda68d6b36ab6d5ae38cfb28f7c9738
jesusdominguez2004 <jesusdominguez2004@gmail.com>
2023-12-21 20:14:08 -0500

    references w3schools

---
+++ w3schools/demo141.cpp
@@ -0,0 +1,13 @@
+// W3Schools, C++ References, Create References
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string food = "Pizza";
+    string &meal = food;
+
+    cout << food << "\n";
+    cout << meal << "\n";
+    return 0;
+}
---
+++ w3schools/demo142.cpp
@@ -0,0 +1,11 @@
+// W3Schools, C++ References, Memory Address
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string food = "Pizza";
+    
+    cout << &food;
+    return 0;
+}

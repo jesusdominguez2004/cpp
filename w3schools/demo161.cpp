ac341a2867d8910a2e682c5da22f219ac9dd9214
jesusdominguez2004 <jesusdominguez2004@gmail.com>
2024-01-05 20:24:00 -0500

    function recursion w3schools

---
+++ w3schools/demo161.cpp
@@ -0,0 +1,17 @@
+// W3Schools, C++ Recursion
+#include <iostream>
+using namespace std;
+
+int sum(int k) {
+    if (k > 0) {
+        return k + sum(k - 1);
+    } else {
+        return 0;
+    }
+}
+
+int main() {
+    int result = sum(10);
+    cout << result;
+    return 0;
+}

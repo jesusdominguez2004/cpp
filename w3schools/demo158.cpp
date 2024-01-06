3d41e33ef82313955d3c3ead4910281b57d9292b
jesusdominguez2004 <jesusdominguez2004@gmail.com>
2024-01-05 20:07:59 -0500

    function overloading w3schools

---
+++ w3schools/demo158.cpp
@@ -0,0 +1,12 @@
+// W3Schools, C++ Function Overloading
+#include <iostream>
+using namespace std;
+
+int myFunction(int x);
+float myFunction(float x);
+double myFunction(double x, double y);
+
+int main() {
+    // ...
+    return 0;
+}

f6211c36c4b1d3c0eaa72ffd4e77c74d0a25ba78
jesusdominguez2004 <jesusdominguez2004@gmail.com>
2024-01-08 22:22:28 -0500

    another pruebas/hello_world

---
+++ pruebas/hello_world/demo24.cpp
@@ -0,0 +1,18 @@
+// hello world...
+#include <iostream>
+using namespace std;
+
+class MyClass {
+    public:
+        void myMethod();
+};
+
+void MyClass::myMethod() {
+    cout << "Hello World!";
+}
+
+int main() {
+    MyClass myObj;
+    myObj.myMethod();
+    return 0;
+}

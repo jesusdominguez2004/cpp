f299b8f9c942d2b6f932d43544891e9b9fe782a1
jesusdominguez2004 <jesusdominguez2004@gmail.com>
2024-06-17 17:53:07 -0500

    2d sand game error

--- pruebas/2d-sand-game/color.h
+++
@@ -1,9 +0,0 @@
-// 2D Sand Game: by "PolarsBear"
-class Color3 {
-    public:
-        Color3(double R, double G, double B);
-        Color3();
-        double R;
-        double G;
-        double B;
-};

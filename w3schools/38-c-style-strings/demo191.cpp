c27d5c0560a023b67fdd5541b20c3ea9922b7d53
jesusdominguez2004 <jesusdominguez2004@gmail.com>
2024-06-26 20:43:20 -0500

    c-style strings w3schools

---
+++ w3schools/38-c-style-strings/demo191.cpp
@@ -0,0 +1,12 @@
+// W3Schools, C++ C-Style Strings
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string greeting1 = "Hello"; // Regular String
+    char greeting2[] = "Hello"; // C-Style String (an array of characters)
+    cout << greeting1 << "\n";
+    cout << greeting2;
+    return 0;
+}
